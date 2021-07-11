d0d0e2f43e7efd488428de8f0f07624184ee95a6
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2021-07-11 06:47:29 -0300

    agregue practicaspato

---
+++ practicaspato/convertidordetemperatura/app.js
@@ -0,0 +1,7 @@
+// Vamos a preguntar la temperatura en Celsius a traves de una ventana emergente (Prompt)
+var temperature = parseInt(prompt ("Cual es la temperatura en celsius?"));
+// Convertir Celsius a Farenheit
+var convertToFarenheit = (temperature*1.8) + 32;
+// Mostrar en la consola a traves de un console.log
+console.log("La temperatura en farenheit es " + convertToFarenheit);
+
---
+++ practicaspato/practicasconmaxi.js
@@ -0,0 +1,79 @@
+// // Hice la carta de Morena a traves de variable que contiene 1 objeto (se define entre llaves).
+// // name y precio son propiedades del objeto.
+
+// const morena = { 
+// name: "morena",
+// precio: 450
+// }
+
+// const pizzamuza = { 
+// name: "muzzarella",
+// precio: 500
+// }
+
+// const pizzanapo = { 
+// name: "napolitana",
+// precio: 750
+// }
+
+// const pizzahumita = { 
+// name: "humita",
+// precio: 850
+// }
+
+// // Creamos un array carrito y un array productos (y dentro de productos tenemos los distintos objetos)
+// let carrito = []
+// let productos = [morena, pizzamuza, pizzahumita, pizzanapo]
+
+// //Despues creamos un objeto y lo guardamos dentro de la variable cliente. 
+// let cliente = {
+// name: "Hernan",
+// carrito: carrito, //un array
+// total: 0
+// }
+// //console.log(productos);
+
+// // Definimos una funcion, declare una funcion.
+// function mostrarcarta(productos) {
+//     console.log("Esta es la carta de Morena "),
+//     console.log(productos)
+// }
+
+// mostrarcarta(productos)
+
+// function agregarAlCarritoDelCliente(miCliente, productoQuePide) {
+//     miCliente.carrito.push(productoQuePide)
+//     console.log(miCliente)
+// }
+
+// agregarAlCarritoDelCliente(cliente, pizzahumita)
+// agregarAlCarritoDelCliente(cliente, pizzamuza)
+// agregarAlCarritoDelCliente(cliente, morena)
+
+// function calcularTotal(params) {
+    
+// }
+
+  //IF
+  // const name = "Patricia";
+  // console.log("Hola" + name);
+  // if (name.lengh > 7) {
+  //   console.log("Que nombre tan largo tienes");
+  // // };
+
+  // const restaurantBill = (bill, friends) => {
+  //   const tax = bill*0.1;
+  //   let theTotal = bill + tax;
+  //   let theTotalHead = theTotal/friends;
+  //   return console.log("El total que debe abonar cada amigo es " + "$ " + theTotalHead.toFixed(2))
+  // };
+
+  // const name = "Patricia";
+  // console.log("Hola " + name);
+  //   if (name.length > 7) {
+  //     console.log("Que nombre tan largo tienes");
+  //   } else {
+  //     console.log("Tu nombre no es muy largo");
+  //   }
+
+  
